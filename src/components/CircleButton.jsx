cd740b98f07b5147704fd4acd2a43cbbb76b9a8f
Yuki <160fe532@gmail.com>
2021-04-18 15:09:20 +0900

    Create MemoDetailScreen

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,40 +1,45 @@
-import React from "react";
-import {View, Text, StyleSheet }from "react-native";
-import { string } from "prop-types";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string, shape } from 'prop-types';
 
 export default function CircleButton(props) {
-    const { children } = props;
-    return (
-        <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>{children}</Text>
-      </View>
-    );
+  const { children, style } = props;
+  return (
+    <View style={[styles.circleButton, style]}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
 }
 
 CircleButton.propTypes = {
-    children: string.isRequired,
+  children: string.isRequired,
+  style: shape(),
+};
+
+CircleButton.defaultProps = {
+  style: null,
 };
 
 const styles = StyleSheet.create({
-    circleButton:{
-        backgroundColor: "#467FD3",
-        width: 64,
-        height: 64,
-        borderRadius: 32,
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-        right: 40,
-        bottom: 40,
-        shadowColor: "#000000",
-        shadowOffset: {width: 0, height: 8},
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-      },
-      circleButtonLabel:{
-        color:"#ffffff",
-        fontSize: 40,
-        lineHeight: 40,
-        elevation: 8, ///androidのみ対応のもの影がつくよ〜
-      }
-})
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+    elevation: 8, ///androidのみ対応のもの影がつくよ〜
+  },
+});
