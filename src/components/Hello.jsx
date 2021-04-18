c2b779f1781ea3d27127ffd1f6865fa2c5ac919f
Yuki <160fe532@gmail.com>
2021-04-18 17:10:05 +0900

    Create MemoEditScreen and MemoCreateScreen

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { string, bool, shape, number } from "prop-types";
+import {
+  string, bool, shape, number 
+} from 'prop-types';
 
 function Hello(props) {
-    const { children, bang, style } = props;
-    return (
-        <View>
-            <Text style={[styles.text, style]}>
-                { `Hello ${children}${bang ? '!' : ''}` }
-            </Text>
-        </View>
-    );
+  const { children, bang, style } = props;
+  return (
+    <View>
+      <Text style={[styles.text, style]}>
+        { `Hello ${children}${bang ? '!' : ''}` }
+      </Text>
+    </View>
+  );
 }
 
 Hello.propTypes = {
-    children: string.isRequired,
-    bang: bool,
-    style: shape(),
+  children: string.isRequired,
+  bang: bool,
+  style: shape(),
 };
 
 Hello.defaultProps = {
-    bang: false,
-    style: null,
+  bang: false,
+  style: null,
 };
 
 const styles = StyleSheet.create({
-    text: {
-        color: "#ffffff",
-        backgroundColor: "blue",
-        fontSize: 40,
-        fontWeight: "bold",
-        padding: 16,
-    },
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
 });
 
 export default Hello;
